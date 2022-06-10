85db8a2e89ec9f69f258307040db50aa40786835
Dana Neuhäusel <skadi@Danas-MacBook-Air.local>
2022-06-10 12:15:19 +0200

    deployment

--- api/users/[userId].js
+++ api/users/[userId].js
@@ -4,7 +4,7 @@ import User from '../../backend/models/User';
 export default async function handler(request, response) {
 	await connectToMongodb();
 	const {userId} = request.query;
-	// TODO: Check for request methods GET and DELETE and handle them!
+
 	if (request.method === 'DELETE') {
 		const deletedUser = await User.findByIdAndRemove(userId);
 		return response.status(200).json({data: deletedUser});
